b01f0c4c897836dd8ec09013d90e2f7b23366160
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2023-09-01 21:37:42 -0300

    Refactorizar y comentar código

--- js/products.js
+++ js/products.js
@@ -4,9 +4,14 @@ const api = "https://japceibal.github.io/emercado-api/cats_products/";
 
 let catId = localStorage.getItem("catID");
 let apiProducts = api + catId + ".json";
-let currentProducts = [];
 let minCount = document.getElementById("rangeFilterCountMin");
 let maxCount = document.getElementById("rangeFilterCountMax");
+
+// Se declara una variable que contendrá el array de productos
+let products = [];
+
+// Esta variable contendrá el nombre de la categoría de productos
+let catName = "";
 
 // Guardamos en una constante el elemento con id lista-productos
 // A este elemento le vamos a agregar los elementos con la información de cada producto como hijo
@@ -15,194 +20,129 @@ const contenedorLista = document.getElementById("lista-productos");
 
 // hacemos fetch al endpoint con la información de los productos y los adjunta al elemento contenedorLista
 
-function getProducts() {
-  fetch(apiProducts)
+function getProductsAndPlaceIntoArr(api) {
+  fetch(api)
     .then((response) => response.json())
     .then((data) => {
-      showProducts(data);
-    });
+      products = data.products;
+      catName = data.catName;
+    })
+    .then(() => displayProducts(products));
 }
 
-// Variable para definir si esta activo el boton de filtro por precio
-let booleanPrecio = false;
-let booleanRelevancia = false;
-
-function filtrarPrecio() {
-  while (contenedorLista.firstChild) {
-    contenedorLista.removeChild(contenedorLista.firstChild);
-  }
-  if (booleanPrecio) {
-    fetch(apiProducts)
-      .then((response) => response.json())
-      .then((data) => {
-        data.products.sort((a, b) => b.cost - a.cost);
-        showProducts(data);
-      });
+// Ordena los productos de forma ascendente o descendente
+function sortProducts(arr, bool, property) {
+  let currentProducts = arr;
+  contenedorLista.innerHTML = "";
+  if (bool) {
+    currentProducts.sort((a, b) => a[property] - b[property]);
+    displayProducts(currentProducts);
   } else {
-    fetch(apiProducts)
-      .then((response) => response.json())
-      .then((data) => {
-        data.products.sort((a, b) => a.cost - b.cost);
-        showProducts(data);
-      });
+    currentProducts.sort((a, b) => b[property] - a[property]);
+    displayProducts(currentProducts);
   }
 }
 
-function filtrarRelevancia() {
-  while (contenedorLista.firstChild) {
-    contenedorLista.removeChild(contenedorLista.firstChild);
-  }
-  if (booleanRelevancia) {
-    fetch(apiProducts)
-      .then((response) => response.json())
-      .then((data) => {
-        data.products.sort((a, b) => b.soldCount - a.soldCount);
-        showProducts(data);
-      });
-  } else {
-    fetch(apiProducts)
-      .then((response) => response.json())
-      .then((data) => {
-        data.products.sort((a, b) => a.soldCount - b.soldCount);
-        showProducts(data);
-      });
-  }
-}
-
+// Recibe un array con productos y los muestra en pantalla
 // Iteramos el array que tiene la info de cada producto, por cada objeto del array creamos un elemento html y los vamos adjuntando a un elemento padre del html
 
-function showProducts(datos) {
-  document.getElementById("nombreCategoria").innerText = datos.catName;
-  for (let i = 0; i < datos.products.length; i++) {
-    {
-      let contenedor = document.createElement("div");
-      contenedor.id = "soyContenedor";
-      contenedor.classList.add("contenedor-producto");
-      contenedor.innerHTML = `
-            <div class="contenedor-imagen">
-                <img class="imagen-producto" src=${datos.products[i].image}>
+function displayProducts(arr) {
+  let divContent = "";
+  for (let i = 0; i < arr.length; i++) {
+    // Desestructuración del objeto actual para que sea más fácil acceder a cada una de sus propiedades.
+    // Sería como guardar las propiedades del objeto en variables
+    let { image, name, currency, cost, description, soldCount } = arr[i];
+    divContent += `
+    <div class="contenedor-producto">
+    <div class="contenedor-imagen">
+                <img class="imagen-producto" src=${image}>
             </div>
     <div class="info-producto">
         <div>
-            <h3 class="datos">${datos.products[i].name} - ${datos.products[i].currency} <span class="price"> ${datos.products[i].cost} </span></h3>
-            <p class="desc">${datos.products[i].description} </p> 
+            <h3 class="datos">${name} - ${currency} ${cost}</h3>
+            <p class="desc">${description} </p> 
         </div>
-        <p class="vendidos">${datos.products[i].soldCount} vendidos</p>
+        <p class="vendidos">${soldCount} vendidos</p>
+    </div>
     </div>
     `;
-      contenedorLista.appendChild(contenedor);
-    }
+  }
+  contenedorLista.innerHTML = divContent;
+  document.getElementById("nombreCategoria").innerText = catName;
+}
+
+function showProductsInPriceRange() {
+  if (minCount.value && maxCount.value) {
+    let currentProducts = products.filter(
+      (e) =>
+        e.cost >= parseInt(minCount.value) && e.cost <= parseInt(maxCount.value)
+    );
+    displayProducts(currentProducts);
+    return currentProducts;
+  }
+  return products;
+}
+
+function changeButtonImage(bool, element, className) {
+  if (bool) {
+    element.classList.remove(`${className}-down`);
+    element.classList.add(`${className}-up`);
+  } else {
+    element.classList.remove(`${className}-up`);
+    element.classList.add(`${className}-down`);
   }
 }
 
-function showProductsByPriceRange() {
-  fetch(apiProducts)
-    .then((response) => response.json())
-    .then((data) => {
-      function showProductsInRange(datos) {
-        contenedorLista.innerHTML = "";
-        document.getElementById("nombreCategoria").innerText = datos.catName;
-
-        if (minCount.value && maxCount.value) {
-          for (let i = 0; i < datos.products.length; i++) {
-            if (
-              data.products[i].cost >= minCount.value &&
-              data.products[i].cost <= maxCount.value
-            ) {
-              {
-                let contenedor = document.createElement("div");
-                contenedor.id = "soyContenedor";
-                contenedor.classList.add("contenedor-producto");
-                contenedor.innerHTML = `
-                <div class="contenedor-imagen">
-                    <img class="imagen-producto" src=${datos.products[i].image}>
-                </div>
-        <div class="info-producto">
-            <div>
-                <h3 class="datos">${datos.products[i].name} - ${datos.products[i].currency} ${datos.products[i].cost}</h3>
-                <p class="desc">${datos.products[i].description} </p> 
-            </div>
-            <p class="vendidos">${datos.products[i].soldCount} vendidos</p>
-        </div>
-        `;
-                contenedorLista.appendChild(contenedor);
-              }
-            }
-          }
-        } else {
-          showProducts(data);
-        }
-      }
-      showProductsInRange(data);
-    });
-}
-// Inicio Buscador.
-
-let buscador = document.getElementById("buscar");
-buscador.addEventListener("keyup", (e) => {
-  let arrayProducts = Array.from(
-    document.getElementsByClassName("contenedor-producto")
-  );
-  arrayProducts.forEach((el) => {
-    !el.textContent.toLowerCase().includes(buscador.value.toLowerCase())
-      ? el.classList.add("buscando")
-      : el.classList.remove("buscando");
-  });
-});
-// Fin Buscador
-
-// Una vez se carga el html, corremos el script
+// Cuando carga el DOM se corre se declaran constantes y variables
+// Se muestran los productos en pantalla
+// Se agregan eventlisteners a los botones y al buscador
 
 document.addEventListener("DOMContentLoaded", () => {
-  let sortCost = document.getElementById("precio");
-  let sortRelevancia = document.getElementById("relevancia");
+  const sortCost = document.getElementById("precio");
+  const sortRelevancia = document.getElementById("relevancia");
+  const priceSortBtn = document.getElementById("sortPrecio");
+  const relSortBtn = document.getElementById("sortRelevancia");
+  const filterBtn = document.getElementById("rangeFilterCount");
+  const clearFilterBtn = document.getElementById("clearRangeFilter");
+  const priceBtnIcon = "fa-sort-numeric";
+  const relBtnIcon = "fa-sort-amount";
+  const searchBar = document.getElementById("buscar");
 
-  getProducts();
+  let priceBool = false;
+  let relBool = false;
+
+  getProductsAndPlaceIntoArr(apiProducts);
+
+  searchBar.addEventListener("keyup", () => {
+    let arrayProducts = Array.from(
+      document.getElementsByClassName("contenedor-producto")
+    );
+    arrayProducts.forEach((el) => {
+      !el.textContent.toLowerCase().includes(searchBar.value.toLowerCase())
+        ? el.classList.add("buscando")
+        : el.classList.remove("buscando");
+    });
+  });
+
   sortCost.addEventListener("click", () => {
-    booleanPrecio = !booleanPrecio;
-    if (booleanPrecio) {
-      document
-        .getElementById("sortPrecio")
-        .classList.remove("fa-sort-numeric-down");
-      document.getElementById("sortPrecio").classList.add("fa-sort-numeric-up");
-    } else {
-      document
-        .getElementById("sortPrecio")
-        .classList.remove("fa-sort-numeric-up");
-      document
-        .getElementById("sortPrecio")
-        .classList.add("fa-sort-numeric-down");
-    }
-    filtrarPrecio();
+    priceBool = !priceBool;
+    changeButtonImage(priceBool, priceSortBtn, priceBtnIcon);
+    sortProducts(showProductsInPriceRange(), priceBool, "cost");
   });
 
   sortRelevancia.addEventListener("click", () => {
-    booleanRelevancia = !booleanRelevancia;
-    if (booleanRelevancia) {
-      document
-        .getElementById("sortRelevancia")
-        .classList.remove("fa-sort-amount-down");
-      document
-        .getElementById("sortRelevancia")
-        .classList.add("fa-sort-amount-up");
-    } else {
-      document
-        .getElementById("sortRelevancia")
-        .classList.remove("fa-sort-amount-up");
-      document
-        .getElementById("sortRelevancia")
-        .classList.add("fa-sort-amount-down");
-    }
-    filtrarRelevancia();
+    relBool = !relBool;
+    changeButtonImage(relBool, relSortBtn, relBtnIcon);
+    sortProducts(showProductsInPriceRange(), relBool, "soldCount");
   });
 
-  document.getElementById("rangeFilterCount").addEventListener("click", () => {
-    showProductsByPriceRange();
+  filterBtn.addEventListener("click", () => {
+    showProductsInPriceRange();
   });
 
-  document.getElementById("clearRangeFilter").addEventListener("click", () => {
+  clearFilterBtn.addEventListener("click", () => {
     minCount.value = "";
     maxCount.value = "";
-    showProductsByPriceRange();
+    displayProducts(products);
   });
 });
