6dcd2f025323a5ca07f427154ab21be801634bc8
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2023-09-08 20:12:35 -0300

    Added Validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,32 +1,49 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+function setDataStorage(email,pass){
     
-    logIn.addEventListener('keyup',(e)=>{
-    if(e.target.classList.contains('form-control')){
+    localStorage.setItem("user", JSON.stringify({ 'mail':email, 'pass': pass }));
+    this.location.href = "./index.html";
+}
+
+function isValid(){
+
+}
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    let inputForm = document.getElementById('logIn');
+    
+    inputForm.addEventListener('keyup',(e)=>{
+        let input = e.target;
+        if(input.classList.contains('form-control')){
         
-        if(e.target.value === ""){
-            e.target.classList.remove('is-valid');
-            e.target.classList.remove('is-invalid');
-        }
-        else if (!e.target.checkValidity()) {
-            e.target.classList.remove('is-valid');
-            e.target.classList.add('is-invalid');
-        } 
-        else if (e.target.checkValidity()) {
-            e.target.classList.remove('is-invalid');
-            e.target.classList.add('is-valid');
-        }            
+            if(input.value === ""){
+                input.classList.remove('is-valid');
+                input.classList.remove('is-invalid');
+            }
+            else if (!input.value.isValid()) {
+                input.classList.remove('is-valid');
+                input.classList.add('is-invalid');
+            } 
+            else if (input.value.isValid()) {
+                input.classList.remove('is-invalid');
+                input.classList.add('is-valid');
+            }            
     }
     });
-    logIn.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('logIn')){
-        if (!logIn.checkValidity()) {
-            e.preventDefault();
-            e.stopPropagation();
-            logIn.classList.add('was-validated')                
+    inputForm.addEventListener('click',(e)=>{
+    
+        let emailValue = document.getElementById('email').value;
+        let passwordValue = document.getElementById('pass').value;
+
+        if(e.target.classList.contains('logIn')){
+            if (!inputForm.checkValidity()) {
+                e.preventDefault();
+                e.stopPropagation();
+                logIn.classList.add('was-validated')                
+            }
+            else{            
+                setDataStorage(emailValue,passwordValue);
+            };                               
         }
-        else{
-        }
-        ;                                
-    }
     });        
 });
