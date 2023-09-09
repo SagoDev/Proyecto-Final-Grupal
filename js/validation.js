5d799d72c1a98e62d019acb65008e207d1927927
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2023-09-09 18:21:36 -0300

    Fix valid data login

--- js/validation.js
+++ js/validation.js
@@ -9,7 +9,19 @@ function setDataStorage(email,pass){
     this.location.href = "./index.html";
 }
 
-function isValid(input){    
+function validData(email,password,e){
+    if(email.value === "" || password.value === "" || email.classList.contains('is-invalid') || password.classList.contains('is-invalid')){
+        e.preventDefault();
+        e.stopPropagation();
+        Swal.fire({
+            icon: 'error',
+            title: 'Debes introducir datos válidos'});                
+    } else {            
+        setDataStorage(email,password);
+    }
+}
+
+function addValidOrInvalidClass(input){    
     
     if(input.value === ""){
         input.classList.remove('is-valid');
@@ -27,30 +39,21 @@ function isValid(input){
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    let inputForm = document.getElementById('logIn');
-    let email = document.getElementById('email');
-    let password = document.getElementById('pass');
+    let form = document.getElementById('logIn');
+    let inputEmail = document.getElementById('email');
+    let inputPassword = document.getElementById('pass');
     let btnLogin = document.getElementById('login');
 
-    inputForm.addEventListener('keyup',(e)=>{
+    form.addEventListener('keyup',(e)=>{
         let input = e.target;
         if(input.classList.contains('form-control')){
-            isValid(input);
+            if(e.keyCode===13){
+                validData(inputEmail,inputPassword,e);
+            }
+            addValidOrInvalidClass(input);
         }
-    });
-    btnLogin.addEventListener('click',(e)=>{
-        
-        if(email.value === "" || password.value === ""){
-            e.preventDefault();
-            e.stopPropagation(); 
-            Swal.fire({
-                icon: 'error',
-                title: 'Debes introducir tus datos!'});            
-
-            isValid(email);
-            isValid(password);
-        } else {
-            setDataStorage(email,password);
-        }        
-    });        
+    });    
+    btnLogin.addEventListener('click',(e)=>{        
+        validData(inputEmail,inputPassword,e);
+    });    
 });
