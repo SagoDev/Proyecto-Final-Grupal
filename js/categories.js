7d9332c35658a532e9ee8dd5ce21f3bbf0e03052
IgnacioPignalosa <ipj1994@gmail.com>
2023-09-13 15:02:20 -0300

    vapai

--- js/categories.js
+++ js/categories.js
@@ -64,7 +64,7 @@ function showCategoriesList() {
         (maxCount != undefined && parseInt(category.productCount) <= maxCount))
     ) {
       htmlContentToAppend += `
-            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
+            <div onclick="setCatID(${category.id})" class="sombra list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
                         <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
