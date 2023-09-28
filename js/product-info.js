e471ff3f370ce31e69d645b1629b2af4aa01b447
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2023-09-28 18:40:22 -0300

    Added slider & Related Prod.

--- js/product-info.js
+++ js/product-info.js
@@ -8,18 +8,22 @@ function traerInfo(api, funcion) {
     .then((Response) => Response.json())
     .then((data) => {
       funcion(data);
+      console.log(api + id + ".json")
     });
 }
 
 function mostrarInfo(info) {
   let contenedor = document.getElementById("contenedor-producto");
   let contenedorImagenes = document.getElementById("contenedor-imagenes");
+  let contenedorCarrusel = document.getElementById("carousel-inner");
+  let titulo = document.getElementById("contenedorTitulo")
+
+  titulo.innerHTML =`
+  <h1 class='pt-2'>${info.name}</h1>
+  <hr>
+  `;
 
   contenedor.innerHTML = `
-        <div class='mt-5'>
-            <h1 class='pt-2'>${info.name}</h1>
-            <hr>
-        </div>
         <div>
             <p> <b>Precio:</b> <br> ${info.currency} ${info.cost}</p>
         </div>
@@ -32,38 +36,39 @@ function mostrarInfo(info) {
         <div>
             <p> <b> Cantidad de vendidos: </b> <br> ${info.soldCount}</p>
         </div>
-        <div>
-            <p> <b>Imagenes ilustrativas: </b></p>
-        </div> 
-    `;
-  for (let image of info.images) {
-    contenedorImagenes.innerHTML += `
+      `;
 
-        <div class='col-3 align-item-center'>
-            <div class='contenedor-imagen-info'>
-                <img class='imagen-info' src=${image}>
-            </div>                
-        </div>        
+  contenedorCarrusel.innerHTML += `
+    <div class="carousel-item active">
+      <img src="${info.images[3]}" class="d-block w-100">
+    </div>       
+  `;
+
+  for (let i = 0; i < info.images.length -1; i++) {
+    contenedorCarrusel.innerHTML += `
+        <div class="carousel-item">
+          <img src="${info.images[i]}" class="d-block w-100">
+        </div>       
         `;
   }
 };
 
 let puntajeEstrellas = '';
 //Función que guarda estrellas en un string según el puntaje, el cual se pasa como parámetro para reutilizar la función
-function creandoEstrellas(puntajeUser){
-  let allStars="";
+function creandoEstrellas(puntajeUser) {
+  let allStars = "";
   for (let i = 0; i < 5; i++) {
-    if(i< puntajeUser){ // si i es menor que el puntaje de estrellas,le agrega una estrella llena
+    if (i < puntajeUser) { // si i es menor que el puntaje de estrellas,le agrega una estrella llena
       allStars += '<i class="bi bi-star-fill"></i>';
-    }else{allStars += '<i class="bi bi-star"></i>';} //cuando ya no se cumple la condicion del if le agrega estrellas vacías al string
-  } 
-  puntajeEstrellas=allStars;
+    } else { allStars += '<i class="bi bi-star"></i>'; } //cuando ya no se cumple la condicion del if le agrega estrellas vacías al string
+  }
+  puntajeEstrellas = allStars;
 }
 function mostrarComments(comentarios) {
   let contenedorComentarios = document.getElementById("commentList");
   for (let comment of comentarios) {
     creandoEstrellas(comment.score);
-    
+
     contenedorComentarios.innerHTML += `
             <div class = "border rounded mb-2 p-2 col">
             <p><b>${comment.user}</b> - ${comment.dateTime} ${puntajeEstrellas} </p>            
@@ -106,6 +111,29 @@ function generarComment() {
         `;
 }
 
+function mostrarRelacionados(info) {
+  let contenedor = document.getElementById('contenedor-relacionados');
+  for (let i of info.relatedProducts) {
+    contenedor.innerHTML += `
+    <div onclick="setProductID(${i.id})" class="col-4">
+        <div class="tarjetas-relacionadas card" style="width: auto;">
+          <img src="${i.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="d-flex justify-content-center">${i.name}</h5>
+          </div>
+        </div>
+    </div>
+        
+     
+    `
+  }
+}
+
+function setProductID(id) {
+  localStorage.setItem("ProductID", id);
+  window.location = "product-info.html";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const enviarComment = document.getElementById("enviar-comment");
   enviarComment.addEventListener("click", () => {
@@ -113,4 +141,5 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   traerInfo(apiProducts, mostrarInfo);
   traerInfo(apiComments, mostrarComments);
+  traerInfo(apiProducts, mostrarRelacionados);
 });
