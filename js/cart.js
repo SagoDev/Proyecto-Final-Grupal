f8c46e87d632d825cc37e7763a6bf97d1fe5f791
Florencia <floestimi129@gmail.com>
2023-10-12 17:27:26 -0300

    html y js arreglado

--- js/cart.js
+++ js/cart.js
@@ -23,57 +23,33 @@ document.addEventListener("DOMContentLoaded", async () => {
     // Es un array con todos los productos
     for (let product of products.articles) {
       content = `
-      <div class="card rounded-3 mb-4">
-        <div class="card-body p-4">
-          <div class="row d-flex align-items-center">
-            <div class="col-md-2 col-lg-2 col-xl-2">
-            </div>
-            <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-3">
-              <p class=" fw-bolder">Nombre</p>
-            </div>
-            <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-2">
-              <p class="fw-bolder">Costo</p>
-            </div>
-            <div class="col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center">
-              <p class="fw-bolder">Cantidad</p>
-            </div>
-            <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-2">
-              <p class="fw-bolder">Subtotal</p>
-            </div>
-            <hr style="height: 2px; background: #000000">
-          </div>
-          <div class="row d-flex align-items-center" id="contenedorDatos">
-            <div class="col-md-2 col-lg-2 col-xl-2">
-              <img src=${product.image} class="rounded-3 mx-auto d-block w-50" alt="imagen del producto">
-            </div>
-            <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-3">
-              <p>${product.name}</p>
-            </div>
-            <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-2">
-              <p>${product.currency} ${product.unitCost}</p>
-            </div>
-            <div class="col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center">
-              <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-              <i class="fas fa-minus" style="color: orange";"></i>
-              </button>
-
-              <input id="form1" min="1" name="quantity" value=${product.count} type="number"
-              class="form-control form-control-sm"/>
-
-              <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-              <i class="fas fa-plus" style="color: orange;"></i>
-              </button>
-            </div>
-            <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-2">
-              <p class="fw-bolder">${product.currency} ${product.unitCost}</p>
-            </div>
-            <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-1 text-end">
-              <a href="#!" style="color: orange;"><i class="bi bi-trash"></i></a>
-            </div>
-          </div>
-          <hr>
+        <div class="col-md-2 col-lg-2 col-xl-2">
+          <img src=${product.image} class="rounded-3 mx-auto d-block w-50" alt="imagen del producto">
         </div>
-      </div>
+        <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-3">
+          <p>${product.name}</p>
+        </div>
+        <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-2">
+          <p>${product.currency} ${product.unitCost}</p>
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center">
+          <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+           <i class="fas fa-minus" style="color: orange";"></i>
+          </button>
+
+          <input id="form1" min="1" name="quantity" value=${product.count} type="number"
+            class="form-control form-control-sm"/>
+
+          <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+            <i class="fas fa-plus" style="color: orange;"></i>
+          </button>
+        </div>
+        <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-2">
+          <p class="fw-bolder">${product.currency} ${product.unitCost}</p>
+        </div>
+        <div class="d-flex justify-content-center col-md-2 col-lg-2 col-xl-1 text-end">
+          <a href="#!" style="color: orange;"><i class="bi bi-trash"></i></a>
+        </div>
     `;
   }
     container.innerHTML = content;
