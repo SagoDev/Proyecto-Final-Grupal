988b91940525f2c6f5a4a4f9e8a04a014d55238e
Laura <laurasoledadrivgomez@hotmail.com>
2023-10-13 13:29:18 -0300

    Pauta1 y desafiate

--- js/product-info.js
+++ js/product-info.js
@@ -20,6 +20,9 @@ function mostrarInfo(info) {
 
   titulo.innerHTML = `
   <h1 class='pt-2'>${info.name}</h1>
+  <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <a href=""><button class="btn btn-primary" type="button" id="comprar">Comprar</button></a>
+</div>
   <hr>
   `;
 
@@ -51,6 +54,34 @@ function mostrarInfo(info) {
         </div>       
         `;
   }
+   // Variable para acceder al botón y otra para el array de productos del carrito
+   let comprarBtn = document.getElementById("comprar");
+   let productosDelCarrito = [];
+
+// Verifico si tengo productos en el localStorage,si los tengo,los convierto en un objeto js y los agrego a una variable
+if (localStorage.getItem("productos")) {
+  productosDelCarrito = JSON.parse(localStorage.getItem("productos"));
+}
+//Función que al hacer click en comprar guardamos la info del producto en un objeto y lo enviamos al array y luego al local storage
+comprarBtn.addEventListener("click", function() {
+  let tituloProduct = info.name;
+  let monedaProducto= info.currency ;
+  let precioUn=info.cost;
+  let imagenSrc = info.images[0];
+  let cantidad=1;
+  // Creamos un objeto con la información que quiero usar del producto
+  let producto = {
+    titulo: tituloProduct,
+    imagenSrc: imagenSrc,
+    moneda: monedaProducto,
+    precioUnidad:precioUn,
+    cantidad:cantidad
+  };
+  // Añadimos el nuevo producto al array
+  productosDelCarrito.push(producto);
+  // Guardamos el array de productos en el localStorage 
+  localStorage.setItem("productos", JSON.stringify(productosDelCarrito));
+});
 }
 
 let puntajeEstrellas = "";
